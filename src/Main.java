912aea0dc29d4c3d0d525dbdd95d37566af2665a
shodgearo <shodkvest@gmail.com>
2018-12-27 17:03:14 +0600

    Create template application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        while (true) {
+            System.out.print("Typed quantity characters (typed 0 for exit): ");
+            int n = scan.nextInt();
+
+            String s = "";
+
+            if (n != 0) {
+                
+            } else break;
+        }
     }
 }
